88c4e5f6f6af2ff6da44c013b2e626a68b2f3e80
Lucccyo <cha.git@mailo.fr>
2023-01-05 15:43:14 +0100

    tests with two arguments and testing two properties

--- croak/bin/main.ml
+++ croak/bin/main.ml
@@ -1,22 +1,6 @@
-type os = 
-  | MacOS
-  | Linux
-  | Win32
-
-let f os = 
-  match os with
-  | MacOS -> Unix.sleep 2
-  | Linux -> Unix.sleep 0
-  | Win32 -> Unix.sleep 0
-
-let os = 
-  let open Crowbar in
-  map[range 3] (fun n ->
-    match n with
-    | 0 -> MacOS
-    | 1 -> Linux
-    | 2 -> Win32
-    | _ -> assert false)
+let f l _a = 
+  List.rev l
 
 let () =
-  Crowbar.(add_test ~name:"identity function" [os] (fun os -> f os))
+  Crowbar.(add_test ~name:"identity A" [list int ; int] (fun l a -> check_eq l (f l a)));
+  Crowbar.(add_test ~name:"identity B" [list int] (fun l -> check_eq l []))
