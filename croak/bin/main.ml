865958feea66b1601899343213f7cd2655c7873a
Lucccyo <cha.git@mailo.fr>
2023-01-09 17:19:07 +0100

    testing contain

--- croak/bin/main.ml
+++ croak/bin/main.ml
@@ -1,12 +1,24 @@
-open Zed_string
-open Uchar
+(* open Zed_string *)
+open Zed_utf8
+(* open Uchar *)
 
-let c_to_utf8 c = of_utf8 (Char.(escaped c))
+(* let c_to_utf8 c = of_utf8 (Char.(escaped c)) *)
 
-let c_to_uchar_to_utf8 c = 
+(* let c_to_uchar_to_utf8 c = 
   let utf8, remaining = of_uChars [of_char c] in
   assert (remaining = []);
-  utf8
+  utf8 *)
+
+(* type zutf8 = Zed_utf8.t *)
+
+let contain_first_char str = 
+  match check str with 
+  | Correct length ->
+    if length > 0 then 
+      let first = sub str 0 1 in
+      assert (contains str first)
+    else ()
+  | Message _m -> () 
 
 let () =
-  Crowbar.(add_test ~name:"testing utf8 conversion" [char] (fun c -> check_eq (c_to_utf8 c) (c_to_uchar_to_utf8 c)))
+  Crowbar.(add_test ~name:"testing utf8 conversion" [bytes] (fun str -> contain_first_char str))
