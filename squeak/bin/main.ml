85af24ec9a462cf9f7a925b4583da325334fa6e3
Lucccyo <cha.git@mailo.fr>
2023-04-12 10:49:16 +0200

    update

--- squeak/bin/main.ml
+++ squeak/bin/main.ml
@@ -13,10 +13,10 @@ type logop = [ `And (** [&] *) | `Or (** [|] *) ]
 (** Prefix operators *)
 type pfxop = [ `Not (** [!] *) | `Defined (** [?] *) ]
 
-type file_name = string
+
 
 (** Source file positions: [(filename, line, column)] *)
-type pos = file_name * int * int
+
 
 (** Environment variable update operators *)
 type env_update_op = Eq       (** [=] *)
@@ -29,14 +29,24 @@ type env_update_op = Eq       (** [=] *)
 
 (** [with_pos] type, used for all units, embedding the element [pelem] ans
       its position [pos] *)
+ *)
+
+(* type value = OpamParserTypes.FullPos.value *)
+
+
+type file_name = string
+
+type pos = file_name * int * int
+
 type 'a with_pos = {
   pelem : 'a;
-  pos : pos\
-} *)
+  pos : pos
+}
 
-type value = OpamParserTypes.FullPos.value
+type value_kind = OpamParserTypes.FullPos.value_kind
+type value = OpamParserTypes.FullPos.value_kind OpamParserTypes.FullPos.with_pos
 
-let value =
+let value_kind =
   let open Crowbar in
   map[range 11] (fun n ->
     match n with
@@ -44,6 +54,9 @@ let value =
     (* | 1 -> OpamParserTypes.FullPos.Int (pos, Random.int 100) *)
     | _ -> assert false)
 
-
 let () =
-  Crowbar.(add_test ~name:"identity function" [value] (fun value -> OpamPrinter.FullPos.format_value Format.std_formatter value))
+  let pos = "", 0, 0 in
+  Crowbar.(add_test ~name:"identity function" [value_kind] (fun value_kind ->
+    let value = {pelem = value_kind; pos} in
+    OpamPrinter.FullPos.format_value Format.std_formatter value
+  ))
