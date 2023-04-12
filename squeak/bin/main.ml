9d481d1058222372fd38a1a232dacdcc0aed9f2c
Lucccyo <cha.git@mailo.fr>
2023-04-12 11:03:11 +0200

    able to generate values

--- squeak/bin/main.ml
+++ squeak/bin/main.ml
@@ -34,7 +34,7 @@ type env_update_op = Eq       (** [=] *)
 (* type value = OpamParserTypes.FullPos.value *)
 
 
-type file_name = string
+(* type file_name = string
 
 type pos = file_name * int * int
 
@@ -44,7 +44,7 @@ type 'a with_pos = {
 }
 
 type value_kind = OpamParserTypes.FullPos.value_kind
-type value = OpamParserTypes.FullPos.value_kind OpamParserTypes.FullPos.with_pos
+type value = OpamParserTypes.FullPos.value_kind OpamParserTypes.FullPos.with_pos *)
 
 let value_kind =
   let open Crowbar in
@@ -55,8 +55,8 @@ let value_kind =
     | _ -> assert false)
 
 let () =
-  let pos = "", 0, 0 in
+  let pos : OpamParserTypes.FullPos.pos = {filename = ""; start = 0, 0; stop = 0,0} in
   Crowbar.(add_test ~name:"identity function" [value_kind] (fun value_kind ->
-    let value = {pelem = value_kind; pos} in
+    let value : OpamParserTypes.FullPos.value= {pelem = value_kind; pos} in
     OpamPrinter.FullPos.format_value Format.std_formatter value
   ))
