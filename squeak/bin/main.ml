be98def84e898c58507c0ba3eb5f7b7118a22c31
Lucccyo <cha.git@mailo.fr>
2023-04-12 16:27:03 +0200

    generate values, but fuzzer doesn't works

--- squeak/bin/main.ml
+++ squeak/bin/main.ml
@@ -1,62 +1,49 @@
-(* * Relational operators
-type relop = [ `Eq  (** [=] *)
-             | `Neq (** [!=] *)
-             | `Geq (** [>=] *)
-             | `Gt  (** [>] *)
-             | `Leq (** [<=] *)
-             | `Lt  (** [<] *)
-             ]
-
-(** Logical operators *)
-type logop = [ `And (** [&] *) | `Or (** [|] *) ]
-
-(** Prefix operators *)
-type pfxop = [ `Not (** [!] *) | `Defined (** [?] *) ]
 
 
+(* type value =
+  | Bool of pos * bool
+  | Int of pos * int
+  | String of pos * string
 
-(** Source file positions: [(filename, line, column)] *)
+  | Relop of pos * relop * value * value
+  | Prefix_relop of pos * relop * value
+  | Logop of pos * logop * value * value
+  | Pfxop of pos * pfxop * value
 
+  | Ident of pos * string
+  | List of pos * value list
+  | Group of pos * value list
+  | Option of pos * value * value list
+  | Env_binding of pos * value * env_update_op * value
+*)
 
-(** Environment variable update operators *)
-type env_update_op = Eq       (** [=] *)
-                   | PlusEq   (** [+=] *)
-                   | EqPlus   (** [=+] *)
-                   | ColonEq  (** [:=] *)
-                   | EqColon  (** [=:] *)
-                   | EqPlusEq (** [=+=] *)
-
-
-(** [with_pos] type, used for all units, embedding the element [pelem] ans
-      its position [pos] *)
- *)
-
-(* type value = OpamParserTypes.FullPos.value *)
-
-
-(* type file_name = string
-
-type pos = file_name * int * int
-
-type 'a with_pos = {
-  pelem : 'a;
-  pos : pos
-}
-
-type value_kind = OpamParserTypes.FullPos.value_kind
-type value = OpamParserTypes.FullPos.value_kind OpamParserTypes.FullPos.with_pos *)
+(* let relop () : OpamParserTypes.FullPos.relop =
+    match Random.int 6 with
+    | 0 ->
+    | 1 -> `Neq
+    | 2 -> `Geq
+    | 3 -> `Gt
+    | 4 -> `Leq
+    | 5 -> `Lt
+    | _ -> assert false *)
 
 let value_kind =
   let open Crowbar in
-  map[range 11] (fun n ->
+  let open OpamParserTypes.FullPos in
+  map[range 4] (fun n ->
     match n with
-    | 0 -> OpamParserTypes.FullPos.Bool (Random.bool ())
-    (* | 1 -> OpamParserTypes.FullPos.Int (pos, Random.int 100) *)
+    | 0 -> Bool (Random.bool ())
+    | 1 -> Int (Random.int 9)
+    | 2 -> String "string"
+    | 3 -> Ident "ident"
+    (* | 3 -> OpamParserTypes.FullPos.Relop (relop ()) value_kind value_kind *)
     | _ -> assert false)
 
 let () =
   let pos : OpamParserTypes.FullPos.pos = {filename = ""; start = 0, 0; stop = 0,0} in
   Crowbar.(add_test ~name:"identity function" [value_kind] (fun value_kind ->
-    let value : OpamParserTypes.FullPos.value= {pelem = value_kind; pos} in
-    OpamPrinter.FullPos.format_value Format.std_formatter value
+    let value : OpamParserTypes.FullPos.value = {pelem = value_kind; pos} in
+    let printed = OpamPrinter.FullPos.value value in
+    Format.printf "[%s]\n" printed;
+    check_eq value (OpamParser.FullPos.value_from_string printed "")
   ))
