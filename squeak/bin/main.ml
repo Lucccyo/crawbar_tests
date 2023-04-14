adec2ffe2906c711e5fa5a47191c7ebf31882441
Lucccyo <cha.git@mailo.fr>
2023-04-14 13:59:28 +0200

    style improvment

--- squeak/bin/main.ml
+++ squeak/bin/main.ml
@@ -2,9 +2,9 @@ type value = OpamParserTypes.FullPos.value_kind OpamParserTypes.FullPos.with_pos
 
 let pp_value ppf ({pelem = value_kind; _} : value) =
   match value_kind with
-  | Int n   -> Format.fprintf ppf "Int %d" n
-  | Bool b  -> Format.fprintf ppf "Bool %b" b
-  | String s-> Format.fprintf ppf "Bool %s" s
+  | Int n    -> Format.fprintf ppf "Int %d" n
+  | Bool b   -> Format.fprintf ppf "Bool %b" b
+  | String s -> Format.fprintf ppf "Bool %s" s
   | _ -> assert false
 
 let gen_value =
@@ -23,8 +23,7 @@ let () =
   let open OpamParserTypes.FullPos in
   let pos = { filename = ""; start = 0,0; stop = 0,0 } in
   Crowbar.(add_test ~name:"identity function" [gen_value] (fun value_kind ->
-    let v = {pelem = value_kind; pos} in
+    let v = { pelem = value_kind; pos } in
     let printed = value v in
-    Format.printf "[%s]\n" printed;
     check_eq ~pp:pp_value ~eq:value_equals v (OpamParser.FullPos.value_from_string printed "")
   ))
